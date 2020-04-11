883bb479e49b7d8697fe56d16796276e5125ea4a
Nikola Tanković <nikola.tankovic@gmail.com>
2020-04-11 19:40:22 +0200

    final week-4

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import storage from './memory_storage.js'
 import cors from 'cors'
+import connect from './db.js'
 
 const app = express()  // instanciranje aplikacije
 const port = 3000  // port na kojem će web server slušati
@@ -21,7 +22,46 @@ app.post('/posts', (req, res) => {
     res.json(data) // vrati podatke za referencu
 })
 
-app.get('/posts', (req, res) => {
+app.get('/posts', async (req, res) => {
+  let db = await connect();
+  let query = req.query;
+
+  let selekcija = {};
+
+  if (query._any) {
+    // za upit: /posts?_all=pojam1 pojam2
+    let pretraga = query._any;
+    let terms = pretraga.split(' ');
+
+    let atributi = ['title', 'createdBy'];
+
+    selekcija = {
+      $and: [],
+    };
+
+    terms.forEach((term) => {
+      let or = {
+        $or: [],
+      };
+
+      atributi.forEach((atribut) => {
+        or.$or.push({ [atribut]: new RegExp(term) });
+      });
+
+      selekcija.$and.push(or);
+    });
+  }
+
+  console.log('Selekcija', selekcija);
+
+  let cursor = await db.collection('posts').find(selekcija);
+  let results = await cursor.toArray();
+
+  res.json(results);
+});
+
+app.get('/posts_memory', (req, res) => {
+
     let posts = storage.posts
     let query = req.query
     
