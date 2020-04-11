111194e3fe9a7fb304bcf025e9ab67b43eec25df
Nikola Tanković <nikola.tankovic@gmail.com>
2020-04-11 20:01:12 +0200

    prettier

--- src/index.js
+++ src/index.js
@@ -1,90 +1,89 @@
 import express from 'express';
-import storage from './memory_storage.js'
-import cors from 'cors'
-import connect from './db.js'
+import storage from './memory_storage.js';
+import cors from 'cors';
+import connect from './db.js';
 
-const app = express()  // instanciranje aplikacije
-const port = 3000  // port na kojem će web server slušati
+const app = express(); // instanciranje aplikacije
+const port = 3000; // port na kojem će web server slušati
 
-app.use(cors())
-app.use(express.json()) // automatski dekodiraj JSON poruke
+app.use(cors());
+app.use(express.json()); // automatski dekodiraj JSON poruke
 
 app.post('/posts', (req, res) => {
-    let data = req.body
+    let data = req.body;
 
     // ovo inače radi baza (autoincrement ili sl.), ali čisto za primjer
-    data.id = 1 + storage.posts.reduce((max, el) => Math.max(el.id, max), 0)
+    data.id = 1 + storage.posts.reduce((max, el) => Math.max(el.id, max), 0);
 
     // dodaj u našu bazu (lista u memoriji)
-    storage.posts.push(data)
+    storage.posts.push(data);
 
     // vrati ono što je spremljeno
-    res.json(data) // vrati podatke za referencu
-})
+    res.json(data); // vrati podatke za referencu
+});
 
 app.get('/posts', async (req, res) => {
-  let db = await connect();
-  let query = req.query;
+    let db = await connect();
+    let query = req.query;
 
-  let selekcija = {};
+    let selekcija = {};
 
-  if (query._any) {
-    // za upit: /posts?_all=pojam1 pojam2
-    let pretraga = query._any;
-    let terms = pretraga.split(' ');
+    if (query._any) {
+        // za upit: /posts?_all=pojam1 pojam2
+        let pretraga = query._any;
+        let terms = pretraga.split(' ');
 
-    let atributi = ['title', 'createdBy'];
+        let atributi = ['title', 'createdBy'];
 
-    selekcija = {
-      $and: [],
-    };
+        selekcija = {
+            $and: [],
+        };
 
-    terms.forEach((term) => {
-      let or = {
-        $or: [],
-      };
+        terms.forEach((term) => {
+            let or = {
+                $or: [],
+            };
 
-      atributi.forEach((atribut) => {
-        or.$or.push({ [atribut]: new RegExp(term) });
-      });
+            atributi.forEach((atribut) => {
+                or.$or.push({ [atribut]: new RegExp(term) });
+            });
 
-      selekcija.$and.push(or);
-    });
-  }
+            selekcija.$and.push(or);
+        });
+    }
 
-  console.log('Selekcija', selekcija);
+    console.log('Selekcija', selekcija);
 
-  let cursor = await db.collection('posts').find(selekcija);
-  let results = await cursor.toArray();
+    let cursor = await db.collection('posts').find(selekcija);
+    let results = await cursor.toArray();
 
-  res.json(results);
+    res.json(results);
 });
 
 app.get('/posts_memory', (req, res) => {
+    let posts = storage.posts;
+    let query = req.query;
 
-    let posts = storage.posts
-    let query = req.query
-    
     if (query.title) {
-        posts = posts.filter(e => e.title.indexOf(query.title) >= 0)
+        posts = posts.filter((e) => e.title.indexOf(query.title) >= 0);
     }
-    
+
     if (query.createdBy) {
-        posts = posts.filter(e => e.createdBy.indexOf(query.createdBy) >= 0)
+        posts = posts.filter((e) => e.createdBy.indexOf(query.createdBy) >= 0);
     }
-    
+
     if (query._any) {
-        let terms = query._any.split(" ")
-        posts = posts.filter(doc => {
-            let info = doc.title + " " + doc.createdBy
-            return terms.every(term => info.indexOf(term) >= 0)
-        })
+        let terms = query._any.split(' ');
+        posts = posts.filter((doc) => {
+            let info = doc.title + ' ' + doc.createdBy;
+            return terms.every((term) => info.indexOf(term) >= 0);
+        });
     }
 
     // sortiranje
-    posts.sort((a, b) => (b.postedAt - a.postedAt))
+    posts.sort((a, b) => b.postedAt - a.postedAt);
 
-    res.json(posts)
-})
+    res.json(posts);
+});
 
-app.listen(port, () => console.log(`Slušam na portu ${port}!`))
+app.listen(port, () => console.log(`Slušam na portu ${port}!`));
